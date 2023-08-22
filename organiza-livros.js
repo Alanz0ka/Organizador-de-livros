be75b9e4df74854aef513ded4abf300e0f1378a2
José Alan <104922063+Alanz0ka@users.noreply.github.com>
2023-08-21 22:30:58 -0300

    teste

---
+++ organiza-livros.js
@@ -0,0 +1,15 @@
+const prompt = require("prompt-sync")
+
+console.log(" \nSeja bem-vindo ao seu organizador de livros, aqui você poderá: \n adicionar, modificar, excluir e pesquisar os seus livros. \n")
+
+class Livro{
+    constructor(titulo, autor, genero, anoPublicacao, sinopse){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.genero = genero;
+        
+
+
+    }
+
+}
